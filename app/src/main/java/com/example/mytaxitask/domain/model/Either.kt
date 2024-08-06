d476ccf40692cd80d844d823f0168ed405f4f090
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-08-06 21:56:40 +0500

    refactor: either

--- app/src/main/java/com/example/mytaxitask/domain/model/Either.kt
+++ app/src/main/java/com/example/mytaxitask/domain/model/Either.kt
@@ -3,6 +3,6 @@ package com.example.mytaxitask.domain.model
 
 sealed interface Either<T> {
     class Right<T>(val data: T) : Either<T>
-    class Left<T>(val errorMessage: Message) : Either<T>
+    class Left<T>(val message: Message) : Either<T>
 
 }
